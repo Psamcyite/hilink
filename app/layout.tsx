c13ab31252dd0cae31dee95fde5d6329d52268f8
Psamcyite <amojoayokunle.com>
2024-02-22 22:11:52 -0600

    initial commit 'Navbar, Footer, Hero'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 import './globals.css'
-// import Navbar from '@/components/Navbar';
-// import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Travel',
@@ -17,11 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/** <Navbar /> */}
+        <Navbar />
         <main className="relative overflow-hidden">
           {children}
         </main>
-        {/** <Footer /> */}
+        <Footer />
       </body>
     </html>
   )
